a32811b158b974328d98fe53f4654314ac8089de
Camilo Moreno <cmcamilomoreno@gmail.com>
2016-03-11 18:22:01 -0500

    Walker is wireless
    - slowing down on decline is a smooth transition
    - next, ultrasonic sensors

--- motor_test/motor_test.ino
+++ motor_test/motor_test.ino
@@ -1,109 +1,89 @@
-#include <Wire.h>
+ #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
 #define BNO055_SAMPLERATE_DELAY_MS (100)
-#define enA 10
-#define in1 9
-#define in2 8
-#define enB 5
-#define in3 7
-#define in4 6
-#define BNO055_SAMPLERATE_DELAY_MS (100)
-#define trig 12
-#define echo 13
-#define touchLeft 4
-#define touchRight 3
 
 Adafruit_BNO055 bno = Adafruit_BNO055();
-boolean motorOn= false; 
-boolean forward = true;
-int dutyCycle=0;
-char inputButtonState;
+ // motor one
+int dir1 = 13;
+int pwm1 = 12;
 
+//motor two
+int dir2 = 11;
+int pwm2 = 10;
 
-void setup()
-{
-// set all the motor control pins to outputs
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+boolean motorOn= false, forward = true;
+
+//Buttons
+char rightB, leftB;
+
+int dutyCycle;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(dir1, OUTPUT);
+  pinMode(dir2, OUTPUT);
+  pinMode(pwm1, OUTPUT);
+  pinMode(pwm2, OUTPUT);
   
-  pinMode(touchLeft,INPUT);
-
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-
-
-  if(!bno.begin())
+  // set both motors to move foward
+  digitalWrite(dir1, HIGH);
+  digitalWrite(dir2, LOW);
+  
+  //buttons
+  pinMode(7,INPUT);
+  pinMode(6,INPUT);
+  
+    if(!bno.begin())
   {
+    /* There was a problem detecting the BNO055 ... check your connections */
     //Serial.write("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
   delay(1000);
   bno.setExtCrystalUse(true);
-}
-
-///////////////////////////////////////////////////////////////////////////
-void loop()
-{
-  //check for button press 
-  inputButtonState = digitalRead(touchLeft);
-
-  if (inputButtonState == LOW && motorOn){     
-    motorOn= !motorOn;
-    dutyCycle = 0;
-  } 
-  else if(inputButtonState == HIGH && !motorOn){
-    motorOn= !motorOn;
-    dutyCycle = 150;
-  }
-    
-  dutyCycle -= adjustForTilt();
-  dutyCycle -= adjustForDistance();
   
-  analogWrite(enA, dutyCycle);
-  analogWrite(enB, dutyCycle);
-  delay(100);
+  Serial.begin(9600);
 }
 
-///////////////////////////////////////////////////////////////////////////
-int adjustForDistance(){
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(8);
-  digitalWrite(trig, LOW);
-  int duration = pulseIn(echo, HIGH, 5000);
-  int distance = constrain(distance, 1, 20);
-  distance = (duration/2) / 20;
+void loop() {
+  // put your main code here, to run repeatedly:
+  rightB = digitalRead(7);
+  leftB = digitalRead(6);
+  dutyCycle=75;
   
-  distance = map(distance,1,20,125,25);
-  return distance;
-}
-
-///////////////////////////////////////////////////////////////////////////
-int adjustForTilt(){
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  
+  if(motorOn){
+     dutyCycle = dutyCycle - map(euler.y(),-2,-30,0,60);
 
-  
-  
-  if(euler.y()>10){
-    return 0;
-  }
-  else if(euler.y()<-10){
-    return 50;
-  }
-  else if(euler.y()>-5 && euler.y()<5){
-    analogWrite(enA, 125);
-    analogWrite(enB, 125);
+//    if(distance<5){
+//      dutyCycle -=50;
+//    }
+//    else if(distance<10){
+//      dutyCycle -=25;
+//    }
+    
+    analogWrite(pwm1, dutyCycle);
+    analogWrite(pwm2, dutyCycle);
     delay(100);
+    Serial.println(dutyCycle);
+   }
+  
+  if ((leftB == LOW || rightB == LOW) && motorOn)
+  {
+    motorOn= !motorOn;
+    analogWrite(pwm1, 0);
+    analogWrite(pwm2, 0);
   }
+  
+  else if((leftB == HIGH && rightB == HIGH) && !motorOn)
+  {
+    motorOn= !motorOn;
+  }
+  
+
 }
 
